da38e90191e84a97258583023393ac7ae071a090
Carolyn <carito.ulfe@gmail.com>
2018-03-10 12:52:46 +0100

    feat: adding actions, reducers and store structure

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Reboot from 'material-ui/Reboot';
 import { MuiThemeProvider } from 'material-ui/styles';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
-
 import App from './components/App/App';
 import theme from './config/theme';
+import store from './config/store';
+
 
 const root = document.getElementById('root');
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-      <Reboot />
-      <App />
-    </MuiThemeProvider>
-    , root);
+  <Provider store={store}>
+    <MuiThemeProvider theme={theme}>
+        <Reboot />
+        <App />
+      </MuiThemeProvider>
+  </Provider>
+  , root);
 
 registerServiceWorker();
