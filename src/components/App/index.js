7894300230b4146de8a04e333f015e9af852c37b
Carolyn <carito.ulfe@gmail.com>
2018-03-15 00:10:06 +0100

    fix: Fixing routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import Button from 'material-ui/Button';
-import './App.css';
 import Routes from '../../config/routes';
 import Dashboard from '../Dashboard';
-
-import { Router, Switch, Route } from 'react-router';
 
 class App extends Component {
   render() {
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,17 +1,20 @@
-import React, { Fragment } from 'react';
-import { Router, Switch, Route } from 'react-router';
+import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Dashboard from '../components/Dashboard';
 
 const Routes = () => (
-    <Switch>
-        <Route exact path="/" component={Dashboard} />
-        <Route path="/categories" component={Dashboard}/>
-        <Route path="/:category/posts" component={Dashboard} />
-        <Route path="/posts"  component={Dashboard} />
-        <Route path="/posts/:id" component={Dashboard}/>
-        <Route path="/posts/:id/comments" component={Dashboard} />
-        <Route path="/comments" component={Dashboard} />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/categories" component={Dashboard}/>
+          <Route path="/:category/posts" component={Dashboard} />
+          <Route path="/posts"  component={Dashboard} />
+          <Route path="/posts/:id" component={Dashboard}/>
+          <Route path="/posts/:id/comments" component={Dashboard} />
+          <Route path="/comments" component={Dashboard} />
+          <Route path="/comments/:id" component={Dashboard} />
+      </Switch>
+    </BrowserRouter>
 );
 
 export default Routes;
